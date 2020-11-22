ca0bac77a2d8a48df5b8ea8859b1b50d8adce5f7
Kevin Samosir <bintangkevin29@gmail.com>
2020-11-22 16:04:47 +0700

    added custom header component

---
+++ components/custom-header/CustomHeader.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {GLOBAL_STYLE_VARIABLES} from '../../constants';
+
+const CustomHeader: React.FC = ({children}) => {
+  return (
+    <View style={style.container}>
+      <Text style={style.text}>{children}</Text>
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    paddingVertical: 27,
+  },
+  text: {
+    color: GLOBAL_STYLE_VARIABLES.whiteColor,
+    fontSize: 18,
+    fontFamily: 'ProductSans-Bold',
+  },
+});
+
+export default CustomHeader;
